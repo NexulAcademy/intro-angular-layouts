3101aab57040f33e164709c4322aaa9438dbc291
Michael Lang <michael.lang@nexul.com>
2018-08-05 22:49:19 -0500

    add some routes and connect to nav links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppNavComponent } from './app-nav/app-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule, MatListModule,
+  MatGridListModule, MatCardModule,
+  MatMenuModule, MatTableModule,
+  MatPaginatorModule, MatSortModule
+} from '@angular/material';
 import { AppDashboardComponent } from './app-dashboard/app-dashboard.component';
 import { CourseTableComponent } from './course-table/course-table.component';
 import { StudentTableComponent } from './student-table/student-table.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppDashboardComponent},
+  { path: 'students', component: StudentTableComponent},
+  { path: 'courses', component: CourseTableComponent}
+];
 
 @NgModule({
   declarations: [
@@ -32,7 +46,8 @@ import { StudentTableComponent } from './student-table/student-table.component';
     MatMenuModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
